705046991ad75d57d51cbec11d7be522af71e84c
zsaaupo <zakariasarker407@gmail.com>
2023-01-25 05:20:49 +0600

    customer basic model added

--- Customer/admin.py
+++ Customer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "full_name",
+        "phone_number"
+    ]
+
+admin.site.register(Customer, CustomerAdmin)
--- Customer/models.py
+++ Customer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+
+    full_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.full_name+" "+"("+self.phone_number+")"

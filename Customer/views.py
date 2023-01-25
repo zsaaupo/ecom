9b4d250c5bc3b33a79ffbe39482728a762b66a24
zsaaupo <zakariasarker407@gmail.com>
2023-01-25 19:00:17 +0600

    rest_framework installed

--- Customer/views.py
+++ Customer/views.py
@@ -2,3 +2,4 @@ from django.shortcuts import render
 
 def sing_up(requrst):
     return render(requrst, "singUp.html")
+
--- Mushroomyan/urls.py
+++ Mushroomyan/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', include("Landing_page.urls")),
     path('user/', include("Customer.urls")),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]

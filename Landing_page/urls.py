c9f37c72dfded39ddacb0cb86fb2a51f1bb88093
zsaaupo <zakariasarker407@gmail.com>
2023-03-10 12:18:18 +0600

    product API created

--- Landing_page/urls.py
+++ Landing_page/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import landin_page
+from .views import landin_page, ProductApi
 
 urlpatterns = [
     path('', landin_page),
+
+    # APIs
+    path('product_api', ProductApi.as_view()),
 ]
--- Landing_page/views.py
+++ Landing_page/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
 def landin_page(request):
     return render(request, "index.html")
+
+class ProductApi(ListAPIView):
+
+    permission_classes = []
+
+    def get(self, request):
+
+        product_data = Product.objects.filter().all()
+        product_data = ProductSerializer(product_data, many=True).data
+
+        return Response(product_data)

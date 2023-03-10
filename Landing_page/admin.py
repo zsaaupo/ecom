cfd7c713602c2e4ee9d78c0bebcb54111a29436a
zsaaupo <zakariasarker407@gmail.com>
2023-03-10 11:14:59 +0600

    created product table

--- Landing_page/admin.py
+++ Landing_page/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "product_name",
+        "product_image",
+        "product_description"
+    ]
+
+admin.site.register(Product,ProductAdmin)
--- Landing_page/models.py
+++ Landing_page/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    product_name = models.CharField(max_length=50)
+    product_image = models.ImageField(upload_to="product/image")
+    product_price = models.CharField(max_length=5)
+    product_description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.product_name
